5221c408ac26df69461b3e7583c07b107416c94e
Manojkumar <manojkumarmariselvam96@gmail.com>
2025-10-01 21:31:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Mano Portfolio 🚀</h1>
+      <p>Welcome to my website!</p>
+    </div>
+  );
+}
+
+export default App;
